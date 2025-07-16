3c1cf0c1d34854734a3dca299fd7a9435e6a4e64
VishalValvi-83 <vishalvalvi2529@gmail.com>
2025-07-16 21:40:48 +0530

    Add initial component structure with properties and methods

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  constructor() {
+    console.log("AppComponent initialized");
+    this.greet();
+  }
+
   title = 'first-ng-app';
+  description = 'This is my first Angular application.';
+
+  //object
+  students = [
+    { id: 1, name: 'JS', age: 20 },
+    { id: 2, name: 'SB', age: 22 },
+    { id: 3, name: 'AK', age: 23 },
+
+  ]
+  //function
+  greet() {
+    console.log("hello world");
+  }
+
+  //function with parameters
+  greetUser(name: string) {
+    console.log(`Hello, vishal!`);
+  }
+  //function with return type
+  getFullName(): string {
+    return "hello world";
+  }
 }
+
