1f59680d5b59c8f91e9dd956ffd8bb0d626b3d61
VishalValvi-83 <vishalvalvi2529@gmail.com>
2025-07-23 00:42:26 +0530

    Feat: Implemented the employee task tracker appliaction

--- Employee-task-tracker/src/app/app.component.ts
+++ Employee-task-tracker/src/app/app.component.ts
@@ -6,5 +6,56 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Employee-task-tracker';
+  title = 'Employee Task Tracker';
+
+  task = {
+    empName: '',
+    title: '',
+    priority: '',
+    dueDate: ''
+  };
+
+  tasks: (typeof this.task)[] = [];
+  noTasks = true;
+  checkNoTasks() {
+    if (this.noTasks) {
+      return 'No tasks available';
+
+    } else {
+      return '';
+
+    }
+  }
+
+  addTask() {
+    if (this.task.empName && this.task.title && this.task.priority && this.task.dueDate) {
+      this.tasks.push({ ...this.task });
+
+      this.task.empName = '';
+      this.task.title = '';
+      this.task.priority = '';
+      this.task.dueDate = '';
+      alert('Task added successfully');
+      console.log(this.tasks);
+    } else {
+      alert('Please fill in all fields');
+    }
+  }
+
+  getPriorityClass(priority: string) {
+    return {
+      'low-priority': priority === 'Low',
+      'medium-priority': priority === 'Medium',
+      'high-priority': priority === 'High'
+    };
+  }
+
+  getFontSize(priority: string): string {
+    if (priority === 'Low') return '14px';
+    if (priority === 'Medium') return '16px';
+    if (priority === 'High') return '18px';
+    return '14px';
+  }
+
+
 }
