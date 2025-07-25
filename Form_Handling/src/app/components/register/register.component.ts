df5bb503417d4c02db05d2cdb58db3e43b27af44
VishalValvi-83 <vishalvalvi2529@gmail.com>
2025-07-25 13:49:52 +0530

    add ts code for register page

--- Form_Handling/src/app/components/register/register.component.ts
+++ Form_Handling/src/app/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
-onRegister(){}
+  user = new FormGroup({
+    fullName: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    phone: new FormControl('', [Validators.required, Validators.maxLength(13), Validators.minLength(10)]),
+    dob: new FormControl('', [Validators.required]),
+    gender: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required, Validators.minLength(8), Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?#&])[A-Za-z\d@$!%*?#&]{8,}$/)]),
+    confirmPassword: new FormControl('', [Validators.required])
+
+
+  })
+  onRegister() {
+    if (this.user.valid) {
+      console.log('Registration Data:', this.user.value);
+    }
+  }
 }
