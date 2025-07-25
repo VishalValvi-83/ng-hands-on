f5bd19b9d4baf21655fc63644128bb982d6e2299
VishalValvi-83 <vishalvalvi2529@gmail.com>
2025-07-25 14:21:32 +0530

    login code

--- Form_Handling/src/app/components/login/login.component.ts
+++ Form_Handling/src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,6 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  User = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(8), Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?#&])[A-Za-z\d@$!%*?#&]{8,}$/)]),
+  })
+
   onLogin() {
 
   }
